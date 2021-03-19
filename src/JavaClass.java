829f4b0b0c59acc0842eb34c49310f4d2f408015
barole000 <bartlomiejolejniczak16@gmail.com>
2021-03-19 19:08:00 +0100

    konflikt

--- src/JavaClass.java
+++ src/JavaClass.java
@@ -2,5 +2,5 @@ public class JavaClass {
     //JDK ver 16.0.0.0
     //JRE ver 8.0.2810.9
     //author DEV1 Bartłomiej Oljeniczak
-    //author DEV2 Daniel Krajenta
+    //author DEV2 Daniel Krajenta 1
 }
