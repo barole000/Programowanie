b8420d3e546b0ebf1debbf161629228d535ad4b6
unknown <dankra001@utp.edu.pl>
2021-03-19 21:37:40 +0100

    poprawa klasy glownej

--- src/JavaClass.java
+++ src/JavaClass.java
@@ -1,6 +1,13 @@
 public class JavaClass {
-    //JDK ver 16.0.0.0
-    //JRE ver 8.0.2810.9
+
+    public static class Hello {
+        public static void main(String[] args) {
+            System.out.println("JDK 16.0.0.0");
+            System.out.println("JRE 8.0.2810.9");
+        }
+    }
+
+
     //author DEV1 Bartłomiej Oljeniczak
     //author DEV2 Daniel Krajenta 1
     //author DEV2 Daniel Krajenta 2
